11c9f5e075682117b46bda21a5ef79cc7d04ced3
donghhan <apensia914@gmail.com>
2022-12-05 14:50:31 +0900

    Tech stacks and tool explanation part edited for index page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import "../styles.css";
 import "nextra-theme-docs/style.css";
 import "@code-hike/mdx/dist/index.css";
-import { ChakraProvider } from "@chakra-ui/react";
 
 export default function Nextra({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
