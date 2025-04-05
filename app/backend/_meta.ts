5ad7f8aa79bfa8e83fae69feb9a92b3e4f6a7fa9
donghhan <apensia914@gmail.com>
2025-04-05 18:44:18 +0700

    Custom permission test code post added

--- app/backend/_meta.ts
+++ app/backend/_meta.ts
@@ -2,4 +2,11 @@ export default {
   authentication: {
     title: "Authentication",
   },
+  _: {
+    type: "separator",
+    title: "Django",
+  },
+  custom_permission_into_generic_views: {
+    title: "Custom permission into generic views",
+  },
 };
